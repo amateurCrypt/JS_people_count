55b8d1ea63d33c1c14582a2d5e7bf8b83e270cad
amateurCrypt <muhammad.qasim1@live.com>
2022-03-02 23:25:22 -0500

    add reduction button

--- index.js
+++ index.js
@@ -6,12 +6,15 @@
 
 let countEl = document.getElementById("count-el");
 
-console.log(countEl);
-
 let count = 0;
 
 function increment () {
-    count = count +1 
+    count = count + 1 ; 
+    countEl.innerText = count;
+}
+
+function reduction () {
+    count = count - 1 ;
     countEl.innerText = count;
 
 }
