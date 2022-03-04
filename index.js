6c312e1fa28e0965fde20de97b2757c3b280e12d
amateurCrypt <muhammad.qasim1@live.com>
2022-03-03 22:36:12 -0500

    small changes

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment () {
-    count = count + 1 ; 
+    count += 1; 
     countEl.innerText = count;
 }
 
 function reduction () {
-    count = count - 1 ;
+    count -= 1;
     countEl.innerText = count;
 
 }
